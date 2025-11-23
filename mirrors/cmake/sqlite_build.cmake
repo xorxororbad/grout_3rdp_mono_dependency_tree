3372ced803358e1894bfa886cc7f395a83def697
badxorxoror <badxorxoror@proton.me>
2025-11-24 00:08:38 +0200

    Some sqlite_build changes

--- mirrors/cmake/sqlite_build.cmake
+++ mirrors/cmake/sqlite_build.cmake
@@ -1,39 +1,40 @@
 include(ExternalProject)
+include(GNUInstallDirs)
 
 set(SQLITE_SOURCE_DIR  "${CMAKE_CURRENT_SOURCE_DIR}/../sqlite")
 set(SQLITE_INSTALL_PATH "${CMAKE_CURRENT_BINARY_DIR}/sqlite-install/")
 set(SQLITE_INSTALL_INCLUDE_PATH "${CMAKE_CURRENT_BINARY_DIR}/sqlite-install/include")
 
+
 file(MAKE_DIRECTORY ${SQLITE_INSTALL_INCLUDE_PATH})
+
+set(ARTIFACT_PATH ${SQLITE_INSTALL_PATH}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}sqlite3${CMAKE_STATIC_LIBRARY_SUFFIX})
 
 ExternalProject_Add(sqlite3_from_src
     SOURCE_DIR          "${CMAKE_CURRENT_LIST_DIR}/../sqlite/"
     INSTALL_DIR         "${SQLITE_INSTALL_PATH}"
     DOWNLOAD_COMMAND    ""
     CONFIGURE_COMMAND
-        <SOURCE_DIR>/configure --prefix=<INSTALL_DIR> --enable-static --disable-shared
+        <SOURCE_DIR>/configure --prefix= --enable-static --disable-shared
     BUILD_COMMAND
         make
     INSTALL_COMMAND
-        make install
+        make install DESTDIR=${SQLITE_INSTALL_PATH}
     
     LOG_CONFIGURE       1
     LOG_BUILD           1
     LOG_INSTALL         1
     
+    CONFIGURE_HANDLED_BY_BUILD TRUE
     USES_TERMINAL_CONFIGURE TRUE
     USES_TERMINAL_BUILD TRUE
+    BUILD_BYPRODUCTS ${ARTIFACT_PATH}
 )
 
 ExternalProject_Add_Step(sqlite3_from_src make-build-dir
     COMMAND ${CMAKE_COMMAND} -E make_directory <BINARY_DIR>
     DEPENDERS configure
 )
-
-#ExternalProject_Add_Step(sqlite3_from_src make-install-include-dir
-#    COMMAND ${CMAKE_COMMAND} -E make_directory <INSTALL_DIR>/include/
-#    DEPENDERS configure
-#)
 
 ExternalProject_Get_Property(sqlite3_from_src INSTALL_DIR)
 
